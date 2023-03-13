7ce4b93b093576dcf7393d3a72efb297791c284c
Me-Rahul-K <102386818+Me-Rahul-K@users.noreply.github.com>
2023-03-13 19:31:13 +0530

    Add files via upload

---
+++ Practice.js
@@ -0,0 +1,43 @@
+// let a = 3;
+// let b = new Number(3);
+// let c = 3;
+
+// console.log( a == b );
+// console.log( a === b );
+// console.log( b === c );
+
+// let  greeting;
+// greetign = {};
+// console.log(greetign);
+
+// let Number = 0;
+// console.log(Number++);
+// console.log(++Number);
+// console.log(Number);
+
+// const sum = eval("10*10+5");
+// console.log(sum);
+
+// var num = 8
+// var num = 10
+// console.log(num)
+
+// console.log(typeof typeof 1);
+
+// const numbers = [1, 2, 3] ;
+// numbers[10] = 11;
+// console.log(numbers);
+
+// console.log("I enrolled in JS" [0]);
+
+// const name = "JSW";
+// console.log(name());
+
+// const one = false || {} || null;
+// const two = null || false || "";
+// const three = [] || 0 || true;
+// console.log(one, two, three);
+
+var x = 3;
+var y = "3";
+console.log(x == y);
